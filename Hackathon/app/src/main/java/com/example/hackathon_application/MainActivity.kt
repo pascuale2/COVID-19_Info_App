0f5ac4020f0122466ced3e61a430e2843eddd04a
pascuale2 <epas97@hotmail.com>
2021-02-01 20:25:34 -0700

    Added additional acitivies & linked them

--- Hackathon/app/src/main/java/com/example/hackathon_application/MainActivity.kt
+++ Hackathon/app/src/main/java/com/example/hackathon_application/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.hackathon_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+   fun goToMaps(view: View) {
+       val intent = Intent(this, Map::class.java)
+       startActivity(intent)
+   }
+
+    fun goToCalendar(view: View) {
+        val intent = Intent(this, Calendar::class.java)
+        startActivity(intent)
+    }
+
+    fun goToStatistics(view: View) {
+        val intent = Intent(this, Statistics::class.java)
+        startActivity(intent)
+    }
+
+    fun goToInfo(view: View) {
+        val intent = Intent(this, Info::class.java)
+        startActivity(intent)
+    }
+
+    fun goToAge(view: View) {
+        val intent = Intent(this, Age::class.java)
+        startActivity(intent)
     }
 }
